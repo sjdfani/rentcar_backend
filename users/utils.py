041e186cd4bbe9643b809647f221e9982524dc2f
Sajjad fani <faniam321@gmail.com>
2024-01-05 20:06:49 +0330

    add user message

--- users/utils.py
+++ users/utils.py
@@ -2,6 +2,29 @@ from rest_framework_simplejwt.tokens import RefreshToken
 import string
 import random
 
+user_messages = {
+    "username_exists": "This username is taken.",
+    "email_exists": "This email is taken.",
+    "password": "Your email or password is incorrect.",
+    "email_not_exists": "This email is not exists.",
+    "role": "You should choose between admin or superuser.",
+    "wrong_password": "Your input password is incorrect.",
+    "equal_passwords": "Your new_password and confirm password isn't equal.",
+    "successful_change_password": "Your password changed.",
+    "unique_id_not_found": "your unique id not found or your code was expired.",
+    "correct_code": "Your input code is correct.",
+    "invalid_code": "Your input code is invalid.",
+    "expire_code": "Your code was expired.",
+    "verify_email": "Your email verified.",
+    "wrong_code": "Your code was expired.",
+    "verified_user": "Your email was verified.",
+    "id_token_invalid": "Your id_token is invalid.",
+}
+
+
+def get_user_messages(title: str):
+    return user_messages.get(title, "")
+
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
