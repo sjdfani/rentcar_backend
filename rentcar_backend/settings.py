dbbfcd856d7692a74dcaf8d794912740b5960f09
Sajjad fani <faniam321@gmail.com>
2024-01-05 19:41:52 +0330

    add new app

--- rentcar_backend/settings.py
+++ rentcar_backend/settings.py
@@ -51,7 +51,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-
+    'users.apps.UsersConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- rentcar_backend/urls.py
+++ rentcar_backend/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('api/v1/users/', include('users.urls'), name='users'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
