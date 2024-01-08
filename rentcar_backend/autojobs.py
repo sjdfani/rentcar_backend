d267035650026db8bf4a4b6b12118aca27fc4cfb
Sajjad fani <faniam321@gmail.com>
2024-01-08 23:01:19 +0330

    add auto job

--- rentcar_backend/autojobs.py
+++ rentcar_backend/autojobs.py
@@ -2,14 +2,15 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import pytz
 from datetime import datetime, timedelta
 from rentcar_backend.settings import env
+from users.models import EmailCode
 
 
 def expire_email_code():
-    # EmailCode.objects.filter(
-    #     status=True,
-    #     updated_at__lte=datetime.now(
-    #         tz=pytz.utc) - timedelta(minutes=env('EXPIRE_EMAIL_CODE', cast=int))
-    # ).update(status=False)
+    EmailCode.objects.filter(
+        status=True,
+        updated_at__lte=datetime.now(
+            tz=pytz.utc) - timedelta(minutes=env('EXPIRE_EMAIL_CODE', cast=int))
+    ).update(status=False)
     pass
 
 
