7748c866d1208c50a91de358ae174839e5d8e591
Sajjad fani <faniam321@gmail.com>
2024-01-20 00:42:18 +0330

    complete admin

--- panel/urls.py
+++ panel/urls.py
@@ -1,5 +1,61 @@
 from django.urls import path
+from .views import (
+    CreateManufacturer,  RetrieveUpdateDestroyManufacturer,
+    CreateCarModel,  RetrieveUpdateDestroyCarModel, CreateCategory,
+    RetrieveUpdateDestroyCategory, CategoryList,
+    RetrieveUpdateDestroyCarOptions, CreateCarOptions, CreateCarYear,
+    RetrieveUpdateDestroyCarYear, CreateCarTemplate, CarTemplateList,
+    RetrieveUpdateDestroyCarTemplate, CreateColor, RetrieveUpdateDestroyColor,
+    CommentList, RetrieveDestroyComment,  GetCarByStatus,
+)
 
 app_name = "panel"
 
-urlpatterns = []
+urlpatterns = [
+    path("manufacturer/create/", CreateManufacturer.as_view(),
+         name="CreateManufacturer"),
+    path("manufacturer/list/<int:pk>/", RetrieveUpdateDestroyManufacturer.as_view(),
+         name="RetrieveUpdateDestroyManufacturer"),
+
+    path("car/model/create/", CreateCarModel.as_view(),
+         name="CreateCarModel"),
+    path("car/model/list/<int:pk>/", RetrieveUpdateDestroyCarModel.as_view(),
+         name="RetrieveUpdateDestroyCarModel"),
+
+    path("category/create/", CreateCategory.as_view(),
+         name="CreateCategory"),
+    path("category/list/", CategoryList.as_view(),
+         name="CategoryList"),
+    path("category/list/<int:pk>/", RetrieveUpdateDestroyCategory.as_view(),
+         name="RetrieveUpdateDestroyCategory"),
+
+    path("car/options/create/", CreateCarOptions.as_view(),
+         name="CreateCarOptions"),
+    path("car/options/list/<int:pk>/", RetrieveUpdateDestroyCarOptions.as_view(),
+         name="RetrieveUpdateDestroyCarOptions"),
+
+    path("year/create/", CreateCarYear.as_view(),
+         name="CreateCarYear"),
+    path("year/list/<int:pk>/", RetrieveUpdateDestroyCarYear.as_view(),
+         name="RetrieveUpdateDestroyCarYear"),
+
+    path("template/create/", CreateCarTemplate.as_view(),
+         name="CreateCarTemplate"),
+    path("template/list/", CarTemplateList.as_view(),
+         name="CarTemplateList"),
+    path("template/list/<int:pk>/", RetrieveUpdateDestroyCarTemplate.as_view(),
+         name="RetrieveUpdateDestroyCarTemplate"),
+
+    path("color/create/", CreateColor.as_view(),
+         name="CreateColor"),
+    path("color/list/<int:pk>/", RetrieveUpdateDestroyColor.as_view(),
+         name="RetrieveUpdateDestroyColor"),
+
+    path("comment/list/", CommentList.as_view(),
+         name="CommentList"),
+    path("comment/list/<int:pk>/", RetrieveDestroyComment.as_view(),
+         name="RetrieveDestroyComment"),
+
+    path("car/list/status/<str:status>/", GetCarByStatus.as_view(),
+         name="GetCarByStatus"),
+]
--- panel/views.py
+++ panel/views.py
@@ -1,3 +1,134 @@
-from django.shortcuts import render
+from rest_framework import generics
+from users.permissions import IsSuperuser
+from car.models import (
+    Manufacturer, CarModel, Category, CarOptions, CarYear, CarTemplate, Car,
+    Color, CarStatus, Comment,
+)
+from car.serializers import (
+    ManufacturerSerializer, CarModelSerializer, CategorySerializer,
+    CarOptionsSerializer, CarYearSerializer, CreateCarTemplateSerializer,
+    CarTemplateSerializer, ColorSerializer, CarSerializer,  CommentSerializer,
+)
 
-# Create your views here.
+
+class CreateManufacturer(generics.CreateAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = ManufacturerSerializer
+    queryset = Manufacturer.objects.all()
+
+
+class RetrieveUpdateDestroyManufacturer(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = ManufacturerSerializer
+    queryset = Manufacturer.objects.all()
+
+
+class CreateCarModel(generics.CreateAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarModelSerializer
+    queryset = CarModel.objects.all()
+
+
+class RetrieveUpdateDestroyCarModel(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarModelSerializer
+    queryset = CarModel.objects.all()
+
+
+class CreateCategory(generics.CreateAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryList(generics.ListAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class RetrieveUpdateDestroyCategory(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CreateCarOptions(generics.CreateAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarOptionsSerializer
+    queryset = CarOptions.objects.all()
+
+
+class RetrieveUpdateDestroyCarOptions(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarOptionsSerializer
+    queryset = CarOptions.objects.all()
+
+
+class CreateCarYear(generics.CreateAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarYearSerializer
+    queryset = CarYear.objects.all()
+
+
+class RetrieveUpdateDestroyCarYear(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarYearSerializer
+    queryset = CarYear.objects.all()
+
+
+class CreateCarTemplate(generics.CreateAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CreateCarTemplateSerializer
+    queryset = CarTemplate.objects.all()
+
+
+class CarTemplateList(generics.ListAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarTemplateSerializer
+    queryset = CarTemplate.objects.all()
+
+
+class RetrieveUpdateDestroyCarTemplate(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarTemplateSerializer
+    queryset = CarTemplate.objects.all()
+
+
+class CreateColor(generics.CreateAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = ColorSerializer
+    queryset = Color.objects.all()
+
+
+class RetrieveUpdateDestroyColor(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = ColorSerializer
+    queryset = Color.objects.all()
+
+
+class CommentList(generics.ListAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+
+class RetrieveDestroyComment(generics.RetrieveDestroyAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+
+class GetCarByStatus(generics.ListAPIView):
+    permission_classes = (IsSuperuser,)
+    serializer_class = CarSerializer
+
+    def get_queryset(self):
+        status = self.kwargs.get("status", None)
+        if status == CarStatus.ACCEPTED:
+            return Car.objects.filter(status=CarStatus.ACCEPTED)
+        elif status == CarStatus.PENDING:
+            return Car.objects.filter(status=CarStatus.PENDING)
+        elif status == CarStatus.REJECTED:
+            return Car.objects.filter(status=CarStatus.REJECTED)
+        return Car.objects.none()
