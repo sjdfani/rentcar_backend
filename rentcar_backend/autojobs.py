61e57f0d99f138e3e374c7437efcdfa4721bf868
Sajjad fani <faniam321@gmail.com>
2024-01-28 00:42:22 +0330

    add auto job

--- rentcar_backend/autojobs.py
+++ rentcar_backend/autojobs.py
@@ -3,6 +3,7 @@ import pytz
 from datetime import datetime, timedelta
 from rentcar_backend.settings import env
 from users.models import EmailCode
+from reserve.models import Reserve, ReserveStatus
 
 
 def expire_email_code():
@@ -11,10 +12,18 @@ def expire_email_code():
         updated_at__lte=datetime.now(
             tz=pytz.utc) - timedelta(minutes=env('EXPIRE_EMAIL_CODE', cast=int))
     ).update(status=False)
-    pass
+
+
+def reject_reserve_requests():
+    Reserve.objects.filter(
+        reserve_status=ReserveStatus.PENDING,
+        created_at__lte=datetime.now(
+            tz=pytz.utc)-timedelta(hours=env('REJECT_RESERVE_REQUEST_HOURS', cast=int))
+    ).update(reserve_status=ReserveStatus.REJECTED)
 
 
 def main():
     scheduler = BackgroundScheduler(timezone='MST')
     scheduler.add_job(expire_email_code, 'interval', seconds=30)
+    scheduler.add_job(reject_reserve_requests, 'interval', seconds=30)
     scheduler.start()
