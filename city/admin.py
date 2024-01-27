5379a76e5e89d0a25de010514e893dc036d3e998
Sajjad fani <faniam321@gmail.com>
2024-01-27 21:24:20 +0330

    complete admin

--- city/admin.py
+++ city/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import City
 
-# Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name", "status")
+    list_filter = ("status",)
+
+
+admin.site.register(City, CityAdmin)
