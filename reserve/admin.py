94cc7aff2d1192b335847495bbf39f50c49b493c
Sajjad fani <faniam321@gmail.com>
2024-01-27 21:23:04 +0330

    complete admin

--- reserve/admin.py
+++ reserve/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Reserve
 
-# Register your models here.
+
+class ReserveAdmin(admin.ModelAdmin):
+    list_display = (
+        "user", "start_rent_date", "reserve_status",
+        "date_of_change_status", "paid_date", "payment_status",
+    )
+    list_filter = ("reserve_status", "payment_status")
+
+
+admin.site.register(Reserve, ReserveAdmin)
