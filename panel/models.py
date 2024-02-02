f931898f3828709d39a9e24218d6a11e2c954752
Sajjad fani <faniam321@gmail.com>
2024-02-03 00:20:29 +0330

    add new model

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from users.models import CustomUser
 
-# Create your models here.
+
+class BasicPaymentInformation(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, verbose_name=_("User"))
+    company_percentage = models.PositiveIntegerField(
+        verbose_name=_("Company_Percentage"))
+    value_added_percentage = models.PositiveIntegerField(
+        verbose_name=_("Value_Added_Percentage"))
+    insurance_percentage = models.PositiveIntegerField(
+        verbose_name=_("Insurance_Percentage"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.user.email
