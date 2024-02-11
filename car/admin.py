ddab73a6c0406389af755f61fedb881cbf758837
Sajjad fani <faniam321@gmail.com>
2024-02-11 18:37:08 +0330

    add new model

--- car/admin.py
+++ car/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "status")
+
+
+admin.site.register(Category, CategoryAdmin)
