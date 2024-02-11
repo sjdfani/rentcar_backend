afec961106955dd40969c9425c3e0856df00b7e5
Sajjad fani <faniam321@gmail.com>
2024-02-11 17:56:45 +0330

    fix bug

--- city/serializers.py
+++ city/serializers.py
@@ -6,3 +6,8 @@ class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = "__all__"
+
+    def validate_name(self, value):
+        if City.objects.filter(name=value).exists():
+            raise serializers.ValidationError("This city is exists.")
+        return value
