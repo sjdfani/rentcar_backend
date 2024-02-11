e17f5e6f569583ca667af99eccd91a89ffb0224e
Sajjad fani <faniam321@gmail.com>
2024-02-11 23:56:12 +0330

    add models

--- car/admin.py
+++ car/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, CarOptions, CarYear, CarModel, Color, Car
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "status")
+    list_display = ("name", "status", "id")
+    list_editable = ("status",)
+
+
+class CarOptionAdmin(admin.ModelAdmin):
+    list_display = ("name", "status", "id")
+    list_editable = ("status",)
+
+
+class CarYearAdmin(admin.ModelAdmin):
+    list_display = ("name", "status", "id")
+    list_editable = ("status",)
+
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ("manufacturers", "name", "id", "pk")
+
+
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ("name", "status", "id")
+    list_editable = ("status",)
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("owner", "status", "id")
+    list_editable = ("status",)
 
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(CarOptions, CarOptionAdmin)
+admin.site.register(CarYear, CarYearAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(Color, ColorAdmin)
+admin.site.register(Car, CarAdmin)
